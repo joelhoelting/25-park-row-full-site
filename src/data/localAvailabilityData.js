5cf14c5b136ee1f97156c2f9cee9dc0313f44f39
Joel Hoelting <joelhoelting@protonmail.com>
2019-01-25 12:04:19 -0500

    Update local availability data to reflect stub data

--- src/data/localAvailabilityData.js
+++ src/data/localAvailabilityData.js
@@ -3,7 +3,7 @@ const availabilityData = `
 	<Listing>
 	<Location>
 		<StreetAddress>20 East End Avenue - Apt: 5B</StreetAddress>
-		<UnitNumber>6B</UnitNumber>
+		<UnitNumber>5A</UnitNumber>
 		<City>Manhattan</City>
 		<State>NY</State>
 		<Zip>10028</Zip>
@@ -16,8 +16,8 @@ const availabilityData = `
 	</Location>
 	<ListingDetails>
 		<Status>Active</Status>
-		<Price>1830000</Price>
-		<MaintenanceCC>1203</MaintenanceCC>
+		<Price>25000000</Price>
+		<MaintenanceCC>9231</MaintenanceCC>
 		<ListingUrl>http://www.corcoran.com/nyc/Listings/Display/5446769</ListingUrl>
 		<Webid>5446769</Webid>
 	</ListingDetails>
@@ -26,11 +26,11 @@ const availabilityData = `
 		<PropertyType>Condo</PropertyType>
 		<PhysicalPropertyType>Apartment</PhysicalPropertyType>
 		<Description>The stately 20 East End, designed by the world-renowned Robert A.M. Stern, seamlessly blends old world elegance with top of the line modern amenities at one of Manhattan's most prestigious addresses. 20 East End Avenue evokes a time when the Astors, Vanderbilts and Rockefellers dominated Manhattan and defined a level of luxury unparalled until now. The pinnacle of 20 East End is Penthouse A, a grand five bedroom, five and a half bathroom full floor simplex residence. With direct dual elevator access opening onto an elegant gallery, a private library, family room, formal dining room and living room, this awe-inspiring home is ideal for entertaining and impressing with over 10,000 square feet of combined indoor and outdoor space. The exclusive rooftop terrace from a bygone era is accessible through an elevator or private stair and includes a wood burning fireplace made of Indiana limestone. Offering unparalleled 360 degree views of Manhattan and the East River, the terrace was designed to support a swimming pool and outdoor kitchen. Additional details include custom lighting and a solid Indiana limestone balustrade and details.  Penthouse A showcases timeless elegance in its exquisite floor plan. Spanning approximately 6,050 interior square feet with 4,542 exterior square feet, this palatial residence features up to 12-foot ceiling heights, three interior wood burning fireplaces, one outdoor wood burning fireplace, expansive sound attenuated casement windows, custom moldings and trim, and solid Appalachian oak herringbone flooring of the finest of quality. The corner living room enjoys a stately wood burning fireplace with long, unobstructed panoramic views to both the north and east with immediate access to the south facing adjacent terrace. The grand formal dining room with a wood burning fireplace can easily accommodate a 16 person dining table and is supported by a complete butler's pantry. There are sparkling views of the city skyline to the south and west and access to two separate terraces. The open chef's kitchen with center island boasts custom shaker inspired millwork cabinetry designed by Robert A.M. Stern, spectacular Calacatta Monarch countertops and backsplash, Waterworks fittings made in France and the complete Gaggenau 400 Series appliance collection including two dishwashers, two warming drawers, two ovens (one convection oven and one combination convection speed oven with microwave), a full height wine refrigerator with dual temperature zones, a Japanese Teppanyaki flat top grill, a five burner cooktop, vented hood and a fully automatic coffee machine built into the kitchen cabinetry. This marvelous property also boasts built-in audio visual, window treatments and lighting controls from wall mounted Control4 panels as well as thermostats in every room, and a programmable and WiFi enabled radiant floor heating system in the master and secondary bathrooms. The stunning master bedroom suite comes equipped with a morning kitchen for convenient access to that first cup of coffee in the morning or a glass of champagne in the evening. The master bedroom features a wood burning fireplace and south and east views, connected to an adjoining dressing room. The master bathroom, flanked by two large walk in closets, is clad in white Caldia Classico marble from Tuscany and extends East through a pair of double doors to a romantic terrace with river views and a hand carved balustrade made of solid Indiana limestone. This spectacular residence offers a rare opportunity to live in the finest full floor penthouse on the Upper East Side.**Immediate Closings**</Description>
-		<Bedrooms>1</Bedrooms>
-		<Bathrooms>1</Bathrooms>
-		<FullBathrooms>1.5</FullBathrooms>
+		<Bedrooms>4</Bedrooms>
+		<Bathrooms>4</Bathrooms>
+		<FullBathrooms>5</FullBathrooms>
 		<HalfBathrooms>1</HalfBathrooms>
-		<LivingArea>1001</LivingArea>
+		<LivingArea>6050</LivingArea>
 	</BasicDetails>
 	<Pictures>
 		<Picture>
@@ -100,7 +100,7 @@ const availabilityData = `
 			<ViewType>City</ViewType>
 			<ViewType>Park</ViewType>
 		</ViewTypes>
-		<Tax_Amount>1203</Tax_Amount>
+		<Tax_Amount>118416</Tax_Amount>
 		<Rent_Sale>S</Rent_Sale>
 		<Hide_Price_yn>N</Hide_Price_yn>
 		<InContract>N</InContract>
@@ -109,7 +109,7 @@ const availabilityData = `
 	<Listing>
 		<Location>
 			<StreetAddress>20 East End Avenue - Apt: 5B</StreetAddress>
-			<UnitNumber>5C</UnitNumber>
+			<UnitNumber>6B</UnitNumber>
 			<City>Manhattan</City>
 			<State>NY</State>
 			<Zip>10028</Zip>
@@ -122,8 +122,8 @@ const availabilityData = `
 		</Location>
 		<ListingDetails>
 			<Status>Active</Status>
-			<Price>2375000</Price>
-			<MaintenanceCC>1610</MaintenanceCC>
+			<Price>25000000</Price>
+			<MaintenanceCC>9231</MaintenanceCC>
 			<ListingUrl>http://www.corcoran.com/nyc/Listings/Display/5446769</ListingUrl>
 			<Webid>5446769</Webid>
 		</ListingDetails>
@@ -132,11 +132,11 @@ const availabilityData = `
 			<PropertyType>Condo</PropertyType>
 			<PhysicalPropertyType>Apartment</PhysicalPropertyType>
 			<Description>The stately 20 East End, designed by the world-renowned Robert A.M. Stern, seamlessly blends old world elegance with top of the line modern amenities at one of Manhattan's most prestigious addresses. 20 East End Avenue evokes a time when the Astors, Vanderbilts and Rockefellers dominated Manhattan and defined a level of luxury unparalled until now. The pinnacle of 20 East End is Penthouse A, a grand five bedroom, five and a half bathroom full floor simplex residence. With direct dual elevator access opening onto an elegant gallery, a private library, family room, formal dining room and living room, this awe-inspiring home is ideal for entertaining and impressing with over 10,000 square feet of combined indoor and outdoor space. The exclusive rooftop terrace from a bygone era is accessible through an elevator or private stair and includes a wood burning fireplace made of Indiana limestone. Offering unparalleled 360 degree views of Manhattan and the East River, the terrace was designed to support a swimming pool and outdoor kitchen. Additional details include custom lighting and a solid Indiana limestone balustrade and details.  Penthouse A showcases timeless elegance in its exquisite floor plan. Spanning approximately 6,050 interior square feet with 4,542 exterior square feet, this palatial residence features up to 12-foot ceiling heights, three interior wood burning fireplaces, one outdoor wood burning fireplace, expansive sound attenuated casement windows, custom moldings and trim, and solid Appalachian oak herringbone flooring of the finest of quality. The corner living room enjoys a stately wood burning fireplace with long, unobstructed panoramic views to both the north and east with immediate access to the south facing adjacent terrace. The grand formal dining room with a wood burning fireplace can easily accommodate a 16 person dining table and is supported by a complete butler's pantry. There are sparkling views of the city skyline to the south and west and access to two separate terraces. The open chef's kitchen with center island boasts custom shaker inspired millwork cabinetry designed by Robert A.M. Stern, spectacular Calacatta Monarch countertops and backsplash, Waterworks fittings made in France and the complete Gaggenau 400 Series appliance collection including two dishwashers, two warming drawers, two ovens (one convection oven and one combination convection speed oven with microwave), a full height wine refrigerator with dual temperature zones, a Japanese Teppanyaki flat top grill, a five burner cooktop, vented hood and a fully automatic coffee machine built into the kitchen cabinetry. This marvelous property also boasts built-in audio visual, window treatments and lighting controls from wall mounted Control4 panels as well as thermostats in every room, and a programmable and WiFi enabled radiant floor heating system in the master and secondary bathrooms. The stunning master bedroom suite comes equipped with a morning kitchen for convenient access to that first cup of coffee in the morning or a glass of champagne in the evening. The master bedroom features a wood burning fireplace and south and east views, connected to an adjoining dressing room. The master bathroom, flanked by two large walk in closets, is clad in white Caldia Classico marble from Tuscany and extends East through a pair of double doors to a romantic terrace with river views and a hand carved balustrade made of solid Indiana limestone. This spectacular residence offers a rare opportunity to live in the finest full floor penthouse on the Upper East Side.**Immediate Closings**</Description>
-			<Bedrooms>2</Bedrooms>
-			<Bathrooms>2.5</Bathrooms>
-			<FullBathrooms>2.5</FullBathrooms>
+			<Bedrooms>4</Bedrooms>
+			<Bathrooms>4</Bathrooms>
+			<FullBathrooms>5</FullBathrooms>
 			<HalfBathrooms>1</HalfBathrooms>
-			<LivingArea>1346</LivingArea>
+			<LivingArea>6050</LivingArea>
 		</BasicDetails>
 		<Pictures>
 			<Picture>
@@ -215,7 +215,7 @@ const availabilityData = `
 	<Listing>
 		<Location>
 			<StreetAddress>20 East End Avenue - Apt: 7C</StreetAddress>
-			<UnitNumber>20A</UnitNumber>
+			<UnitNumber>11C</UnitNumber>
 			<City>Manhattan</City>
 			<State>NY</State>
 			<Zip>10028</Zip>
@@ -228,8 +228,8 @@ const availabilityData = `
 		</Location>
 		<ListingDetails>
 			<Status>Active</Status>
-			<Price>3000000</Price>
-			<MaintenanceCC>1994</MaintenanceCC>
+			<Price>15000000</Price>
+			<MaintenanceCC>5330</MaintenanceCC>
 			<ListingUrl>http://www.corcoran.com/nyc/Listings/Display/5337041</ListingUrl>
 			<Webid>5337041</Webid>
 		</ListingDetails>
@@ -238,11 +238,11 @@ const availabilityData = `
 			<PropertyType>Condo</PropertyType>
 			<PhysicalPropertyType>Apartment</PhysicalPropertyType>
 			<Description>**Immediate Closings**Well renowned architect, Robert A. M. Stern, envisioned 20 East End as his signature building - a bespoke property emanating luxury and elegance. One-of-a-kind residence 7C, a sweeping 4 bedroom and 4 and one half bathroom duplex, is certainly testimony to that vision. Offering North, South and West exposures, 7C showcases timeless elegance in its grandiose floor plan. Spanning approximately 4,000 interior square feet, and approximately 1,465 exterior square feet, this palatial residence features 11 foot ceilings, expansive casement windows, custom moldings, 8 foot doorways and Appalachian oak herringbone flooring with a walnut stain. The lower floor includes a grand living room, formal dining room that leads to the immense terrace with a stunning gas burning fireplace, eat-in-kitchen and 4th bedroom. The chef's kitchen boasts custom tailored shaker inspired millwork cabinetry, natural Brazilian quartzite countertops, Lefroy Brooks hardware, Gaggenau appliances, and more than ample storage space. The second level is home to three of the four bedrooms with ensuite bathrooms as well as a library tucked away from the entertaining space. A stunning master bathroom, fully clad in white Caldia Classico marble, features a cast iron tub, Toto water closet, a RAMSA custom designed double sink vanity and Wi Fi enabled heated flooring. This spectacular residence offers a rare opportunity to live in one of the finest condominiums in Manhattan.</Description>
-			<Bedrooms>2</Bedrooms>
-			<Bathrooms>2.5</Bathrooms>
-			<FullBathrooms>2.5</FullBathrooms>
+			<Bedrooms>3</Bedrooms>
+			<Bathrooms>3</Bathrooms>
+			<FullBathrooms>3</FullBathrooms>
 			<HalfBathrooms>1</HalfBathrooms>
-			<LivingArea>1558</LivingArea>
+			<LivingArea>3985</LivingArea>
 		</BasicDetails>
 		<Pictures>
 			<Picture>
@@ -323,7 +323,7 @@ const availabilityData = `
 	<Listing>
 		<Location>
 			<StreetAddress>20 East End Avenue - Apt: 7C</StreetAddress>
-			<UnitNumber>27C</UnitNumber>
+			<UnitNumber>10E</UnitNumber>
 			<City>Manhattan</City>
 			<State>NY</State>
 			<Zip>10028</Zip>
@@ -336,8 +336,8 @@ const availabilityData = `
 		</Location>
 		<ListingDetails>
 			<Status>Active</Status>
-			<Price>3765000</Price>
-			<MaintenanceCC>2372</MaintenanceCC>
+			<Price>10000000</Price>
+			<MaintenanceCC>5330</MaintenanceCC>
 			<ListingUrl>http://www.corcoran.com/nyc/Listings/Display/5337041</ListingUrl>
 			<Webid>5337041</Webid>
 		</ListingDetails>
@@ -346,12 +346,11 @@ const availabilityData = `
 			<PropertyType>Condo</PropertyType>
 			<PhysicalPropertyType>Apartment</PhysicalPropertyType>
 			<Description>**Immediate Closings**Well renowned architect, Robert A. M. Stern, envisioned 20 East End as his signature building - a bespoke property emanating luxury and elegance. One-of-a-kind residence 7C, a sweeping 4 bedroom and 4 and one half bathroom duplex, is certainly testimony to that vision. Offering North, South and West exposures, 7C showcases timeless elegance in its grandiose floor plan. Spanning approximately 4,000 interior square feet, and approximately 1,465 exterior square feet, this palatial residence features 11 foot ceilings, expansive casement windows, custom moldings, 8 foot doorways and Appalachian oak herringbone flooring with a walnut stain. The lower floor includes a grand living room, formal dining room that leads to the immense terrace with a stunning gas burning fireplace, eat-in-kitchen and 4th bedroom. The chef's kitchen boasts custom tailored shaker inspired millwork cabinetry, natural Brazilian quartzite countertops, Lefroy Brooks hardware, Gaggenau appliances, and more than ample storage space. The second level is home to three of the four bedrooms with ensuite bathrooms as well as a library tucked away from the entertaining space. A stunning master bathroom, fully clad in white Caldia Classico marble, features a cast iron tub, Toto water closet, a RAMSA custom designed double sink vanity and Wi Fi enabled heated flooring. This spectacular residence offers a rare opportunity to live in one of the finest condominiums in Manhattan.</Description>
-			<Bedrooms>2</Bedrooms>
-			<Bathrooms>2.5</Bathrooms>
+			<Bedrooms>3</Bedrooms>
+			<Bathrooms>2</Bathrooms>
 			<FullBathrooms>4</FullBathrooms>
 			<HalfBathrooms>1</HalfBathrooms>
-			<LivingArea>1725</LivingArea>
-			<ExtLivingArea>278</ExtLivingArea>
+			<LivingArea>3985</LivingArea>
 		</BasicDetails>
 		<Pictures>
 			<Picture>
@@ -432,7 +431,7 @@ const availabilityData = `
 	<Listing>
 		<Location>
 			<StreetAddress>20 East End Avenue - Apt: M</StreetAddress>
-			<UnitNumber>37B</UnitNumber>
+			<UnitNumber>22A</UnitNumber>
 			<City>Manhattan</City>
 			<State>NY</State>
 			<Zip>10028</Zip>
@@ -445,8 +444,8 @@ const availabilityData = `
 		</Location>
 		<ListingDetails>
 			<Status>Active</Status>
-			<Price>5385000</Price>
-			<MaintenanceCC>3190</MaintenanceCC>
+			<Price>5000000</Price>
+			<MaintenanceCC>5661</MaintenanceCC>
 			<ListingUrl>http://www.corcoran.com/nyc/Listings/Display/5337141</ListingUrl>
 			<Webid>5337141</Webid>
 		</ListingDetails>
@@ -455,12 +454,11 @@ const availabilityData = `
 			<PropertyType>Condo</PropertyType>
 			<PhysicalPropertyType>Apartment</PhysicalPropertyType>
 			<Description>**Immediate Closings**20 East End Avenue's breathtaking, three-story Maisonette is a prized residence that offers the autonomy of a private home with the amenities of a full-service building. Spanning approximately 4,737 interior square feet, this palatial residence features soaring ceilings, expansive casement windows, custom moldings, and Appalachian oak herringbone flooring with walnut stain. Designed in the spirit of classical New York City townhomes, the Maisonette offers a gracious entry gallery and grand staircase that connects entertaining and living spaces with private quarters above.  This residence features a large chef's kitchen, a separate dining room, a library which can also function as a fifth bedroom, and a majestic living room with exquisite natural light.  A large utility room with sink is located on the lower level, while upstairs are four generously sized bedrooms with en-suite baths.  This elegant residence offers the opportunity to own a bespoke home in Robert A. M. Stern's signature building.</Description>
-			<Bedrooms>3</Bedrooms>
-			<Bathrooms>3.5</Bathrooms>
+			<Bedrooms>2</Bedrooms>
+			<Bathrooms>2</Bathrooms>
 			<FullBathrooms>2</FullBathrooms>
 			<HalfBathrooms>0</HalfBathrooms>
-			<LivingArea>2187</LivingArea>
-			<ExtLivingArea>319</ExtLivingArea>
+			<LivingArea>4737</LivingArea>
 		</BasicDetails>
 		<Pictures>
 			<Picture>
@@ -541,7 +539,7 @@ const availabilityData = `
 	<Listing>
 		<Location>
 			<StreetAddress>20 East End Avenue - Apt: M</StreetAddress>
-			<UnitNumber>42A</UnitNumber>
+			<UnitNumber>45A</UnitNumber>
 			<City>Manhattan</City>
 			<State>NY</State>
 			<Zip>10028</Zip>
@@ -554,8 +552,8 @@ const availabilityData = `
 		</Location>
 		<ListingDetails>
 			<Status>Active</Status>
-			<Price>12500000</Price>
-			<MaintenanceCC>5859</MaintenanceCC>
+			<Price>5000000</Price>
+			<MaintenanceCC>5661</MaintenanceCC>
 			<ListingUrl>http://www.corcoran.com/nyc/Listings/Display/5337141</ListingUrl>
 			<Webid>5337141</Webid>
 		</ListingDetails>
@@ -564,12 +562,11 @@ const availabilityData = `
 			<PropertyType>Condo</PropertyType>
 			<PhysicalPropertyType>Apartment</PhysicalPropertyType>
 			<Description>**Immediate Closings**20 East End Avenue's breathtaking, three-story Maisonette is a prized residence that offers the autonomy of a private home with the amenities of a full-service building. Spanning approximately 4,737 interior square feet, this palatial residence features soaring ceilings, expansive casement windows, custom moldings, and Appalachian oak herringbone flooring with walnut stain. Designed in the spirit of classical New York City townhomes, the Maisonette offers a gracious entry gallery and grand staircase that connects entertaining and living spaces with private quarters above.  This residence features a large chef's kitchen, a separate dining room, a library which can also function as a fifth bedroom, and a majestic living room with exquisite natural light.  A large utility room with sink is located on the lower level, while upstairs are four generously sized bedrooms with en-suite baths.  This elegant residence offers the opportunity to own a bespoke home in Robert A. M. Stern's signature building.</Description>
-			<Bedrooms>4</Bedrooms>
-			<Bathrooms>4.5</Bathrooms>
+			<Bedrooms>5</Bedrooms>
+			<Bathrooms>5</Bathrooms>
 			<FullBathrooms>2</FullBathrooms>
 			<HalfBathrooms>0</HalfBathrooms>
-			<LivingArea>3580</LivingArea>
-			<ExtLivingArea>3580</ExtLivingArea>
+			<LivingArea>4737</LivingArea>
 		</BasicDetails>
 		<Pictures>
 			<Picture>
