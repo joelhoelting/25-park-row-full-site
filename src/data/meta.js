77451062e1180e2822b7a619b5c158158cd9774a
Joel Hoelting <joelhoelting@protonmail.com>
2019-02-21 18:44:09 -0500

    Add React Helmet and update meta/title description for each page

---
+++ src/data/meta.js
@@ -0,0 +1,40 @@
+const metaData = {
+  home: {
+    title: '25 Park Row',
+    description: '25 Park Row is a new architectural icon providing unmatched Downtown NYC views from every luxury condominium home over historic City Hall Park.'
+  },
+  architecture: {
+    title: '25 Park Row | Architecture',
+    description: 'At 25 Park Row, the classic art deco condominium that overlooks City Hall Park is reinterpreted with cascading terraces, setbacks and loggias. Designed by COOKFOX Architects.'
+  },
+  interiors: {
+    title: '25 Park Row | Interiors',
+    description: 'With vistas stretching over City Hall Park, each condo at 25 Park Row creates a connection between fine interior finishes and breathtaking views of Downtown NYC.'
+  },
+  amenities: {
+    title: '25 Park Row | Amenities',
+    description: '25 Park Row condominium owners can exclusive enjoy The Park Row Club - a richly appointed luxury amenity suite inspired by the Gilded Age. Over 7,000 square feet of entertainment.'
+  },
+  views: {
+    title: '25 Park Row | Views',
+    description: '25 Park Row is a 21st century architectural icon providing unmatched downtown NYC views from every condominium over historic City Hall Park.',
+  },
+  floorplans: {
+    title: '25 Park Row | Floorplans',
+    description: 'The new City Hall Park condos at 25 Park Row provides unmatched Downtown Manhattan views. Spacious 1-4 bedroom and penthouse layouts available.',
+  },
+  neighborhood: {
+    title: '25 Park Row | Neighborhood',
+    description: '25 Park Row’s location on City Hall Park offers the best of the new downtown at every turn. Luxury Downtown NYC 3 and 4-bedroom condos for sale.',
+  },
+  gallery: {
+    title: '25 Park Row | Gallery',
+    description: '25 Park Row is a 21st century architectural icon providing unmatched views over City Hall Park. Spacious 1-4 bedroom and penthouse layouts available.',
+  },
+  contact: {
+    title: '25 Park Row | Contact',
+    description: '25 Park Row’s new luxury Downtown Manhattan condominium provides unmatched luxury amenities with The Park Row Club and over 7,000 square feet of entertainment. Contact our sales gallery below.',
+  }
+};
+
+export default metaData;
