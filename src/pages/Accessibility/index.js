fa860537bb0a2f12c0df9a9624973b70d82d0273
Joel Hoelting <joelhoelting@protonmail.com>
2019-04-22 17:36:31 -0400

    Fix type and remove word from accessibility statement

--- src/pages/Accessibility/index.js
+++ src/pages/Accessibility/index.js
@@ -11,11 +11,13 @@ const AccessibilityInline = {
     textAlign: 'center',
     margin: '0 auto',
     width: '90%',
-    fontSize: '.8rem',
-    height: 'calc(100vh - 300px)',
-    fontFamily: 'Arial,Helvetica Neue,Helvetica,sans-serif',
+    height: 'calc(100vh - 30%)',
     h1: {
       fontFamily: 'Arial,Helvetica Neue,Helvetica,sans-serif',
+      fontSize: '1.2rem',
+      [mediaQueries.tablet]: {
+        fontSize: '1.6rem'
+      },
       [mediaQueries.desktop]: {
         fontSize: '2rem'
       }
@@ -24,6 +26,10 @@ const AccessibilityInline = {
       fontFamily: 'Arial,Helvetica Neue,Helvetica,sans-serif',
       maxWidth: '1200px',
       textAlign: 'left',
+      fontSize: '.8rem',
+      [mediaQueries.tablet]: {
+        fontSize: '1.6rem'
+      },
       [mediaQueries.desktop]: {
         fontSize: '1.6rem'
       }
@@ -40,7 +46,7 @@ const Accessibility = () => {
         Park Row 23 Owners LLC, is committed to providing a website that is accessible to the widest possible audience,
         regardless of technology or ability. We aim to comply with all applicable standards. If you experience any
         difficulty in accessing any part of this website, please contact us by emailing info@25parkrow.com or calling us
-        at 646-933-4625. Link to accessibility statement.
+        at 646-933-4625.
       </p>
     </div>
   );
