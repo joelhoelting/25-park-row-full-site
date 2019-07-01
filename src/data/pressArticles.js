ab0f411a76294219c5e6c865c2f354044cd12a25
Joel Hoelting <joelhoelting@protonmail.com>
2019-07-01 10:49:29 -0400

    Update local press data in case of CMS failure

--- src/data/pressArticles.js
+++ src/data/pressArticles.js
@@ -1,29 +1,29 @@
 const pressArticles = [
   {
-    publisher: 'Curbed NY',
-    title: 'New looks at COOKFOX’s J&R-replacing Financial District skyscraper',
-    date: '2018-08-27T00:00:00.000Z',
-    url: 'https://ny.curbed.com/2018/8/27/17784970/financial-district-25-park-row-cookfox-rendering',
-    body:
-      'Elit elit qui proident quis ullamco adipisicing. Anim ex sint mollit magna fugiat qui in ex labore fugiat. Dolor ea aliqua ut cillum sit enim eu. Excepteur reprehenderit proident exercitation consectetur irure pariatur elit. Cillum aute aliquip aliquip ex nulla ea adipisicing. Culpa nisi enim elit amet dolore anim Lorem cillum.',
-    _id: '5c620f987f91c209db135840',
-    createdAt: '2019-02-12T00:13:12.166Z',
-    updatedAt: '2019-04-22T17:43:03.365Z',
-    __v: 0,
-    id: '5c620f987f91c209db135840'
-  },
-  {
     publisher: 'Mansion Global',
     title: 'Manhattan and Brooklyn’s Newest Condos Have Old New York Charm',
     date: '2018-09-17T00:00:00.000Z',
     url: 'https://www.mansionglobal.com/articles/manhattan-and-brooklyn-s-newest-condos-have-old-new-york-charm-108763',
     body:
-      'Amet elit fugiat laboris consequat cupidatat exercitation adipisicing dolore veniam fugiat veniam. Labore qui mollit ut commodo nostrud quis deserunt occaecat sunt consequat fugiat reprehenderit commodo. In officia eiusmod aliqua veniam deserunt fugiat amet. Veniam eiusmod non occaecat enim excepteur incididunt reprehenderit aute tempor voluptate nostrud.',
+      'This development is located on the previous site of the landmark J&R store, across the street from City Hall Park. The project, created by L + M Development Partners Inc., alongside the J&R Music World family, with architecture COOKFOX will have 110 units ranging from one to five bedrooms, with views of City Hall Park, the Hudson River, the Brooklyn Bridge, One World Trade Center, and the Woolworth Building.',
     _id: '5c620fb97f91c209db135843',
     createdAt: '2019-02-12T00:13:45.766Z',
-    updatedAt: '2019-04-22T17:43:40.838Z',
+    updatedAt: '2019-05-09T18:21:31.239Z',
     __v: 0,
     id: '5c620fb97f91c209db135843'
+  },
+  {
+    publisher: 'Off The MRKT',
+    title: '25 Park Row Launches New Sales',
+    date: '2019-02-04T04:00:00.000Z',
+    url: 'https://www.offthemrkt.com/blogs/25-park-row-launches-new-sales',
+    body:
+      '25 Park Row, the 50-story luxury condo on the former J&R Music & Computer World Site developed by the Family behind J&R Music World and L&M Development Partners Inc, overlooking City Hall Park.   COOKFOX Architects has also designed the interiors for 25 Park Row, which feature 110 residences ranging from one- to five-bedrooms from 900 square feet to over 3,500 interior square feet.',
+    _id: '5cd470a152213e0562cc3626',
+    createdAt: '2019-05-09T18:25:37.803Z',
+    updatedAt: '2019-05-09T18:26:15.290Z',
+    __v: 0,
+    id: '5cd470a152213e0562cc3626'
   },
   {
     publisher: 'NYPost.com',
@@ -31,10 +31,10 @@ const pressArticles = [
     date: '2018-09-19T00:00:00.000Z',
     url: 'https://nypost.com/2018/09/19/the-10-most-hyped-new-nyc-buildings/',
     body:
-      'Consequat ut minim esse anim laboris amet occaecat cupidatat consectetur. Incididunt aliqua dolore fugiat nulla quis qui. Ipsum laboris aliquip anim eiusmod tempor commodo fugiat culpa proident elit tempor tempor id. Nisi dolor laboris ipsum laboris quis magna culpa pariatur.',
+      'In the typical New York way, a long-standing icon is being replaced by a condo. The\nFinancial District’s popular J&R Music World, which closed in 2014 after decades in business, is giving way to the 110-unit 25 Park Row. L+M Development Partners, along\nwith J&R owners Joe and Rachelle Friedman, teamed up for this project, which ranges\nfrom one-to five-bedrooms.',
     _id: '5c620fd37f91c209db135846',
     createdAt: '2019-02-12T00:14:11.259Z',
-    updatedAt: '2019-04-22T17:43:58.785Z',
+    updatedAt: '2019-05-09T18:17:22.977Z',
     __v: 0,
     id: '5c620fd37f91c209db135846'
   },
@@ -45,10 +45,10 @@ const pressArticles = [
     url:
       'https://www.nytimes.com/2018/10/29/realestate/a-former-downtown-music-mecca-becomes-condos.html?rref=collection%2Fsectioncollection%2Frealestate',
     body:
-      'Aute dolor sunt anim adipisicing occaecat quis non cupidatat nulla magna elit. Deserunt Lorem nulla irure ipsum culpa. Cillum anim ipsum cillum enim aute nulla cupidatat consectetur consequat labore. Anim qui fugiat aute eiusmod sit incididunt sunt mollit exercitation exercitation id minim voluptate in. Lorem culpa voluptate aliqua ex anim officia incididunt laborum ex ad exercitation nisi. Ad ad duis incididunt labore velit cupidatat aliqua occaecat incididunt incididunt minim. Mollit incididunt excepteur consectetur sint.',
+      'Like a file recovered from a hard drive, Manhattan’s Park Row—the longtime home\nof the J&R electronics empire—is coming back to life, this time as luxury housing. A soaring new condo, 25 Park Row, is going up on the block across from City Hall\nthat J&R Music and Computer World largely controlled for decades, but which has been\nquiet since the retailer shuttered its last store in 2014.',
     _id: '5c620ff47f91c209db135849',
     createdAt: '2019-02-12T00:14:44.295Z',
-    updatedAt: '2019-04-22T17:44:14.041Z',
+    updatedAt: '2019-05-09T18:16:07.275Z',
     __v: 0,
     id: '5c620ff47f91c209db135849'
   },
@@ -58,10 +58,10 @@ const pressArticles = [
     date: '2018-10-30T00:00:00.000Z',
     url: 'http://fieldcondition.com/blog/2018/10/30/construction-tour-25-park-row-cookfox',
     body:
-      'Magna laborum sit quis et sit officia non nostrud quis enim eu. Non excepteur anim excepteur voluptate ex aliqua exercitation est aute duis irure cillum exercitation reprehenderit. Sit ullamco qui Lorem sint eiusmod duis laboris Lorem labore nostrud occaecat.',
+      "Superstructure is close to topping out at L+M Development Partners Inc. and The\nFamily Behind J&R Music World's 25 Park Row, a 50 story residential condo tower that\nis part of a multi-building development that replaces the J&R Music and Computer\nWorld megastore. Designed by COOKFOX Architects, the tower is clad in limestone colored panels that are likely concrete. When completed, the tower will offer 110\nresidential condo units with views out onto City Hall Park from large windows.",
     _id: '5c6210127f91c209db13584c',
     createdAt: '2019-02-12T00:15:14.450Z',
-    updatedAt: '2019-04-22T17:44:24.764Z',
+    updatedAt: '2019-05-09T18:14:50.389Z',
     __v: 0,
     id: '5c6210127f91c209db13584c'
   },
@@ -72,10 +72,10 @@ const pressArticles = [
     url:
       'https://www.architecturaldigest.com/story/andre-mellone-discusses-art-deco-modern-amenities-and-designer-hacks',
     body:
-      'Tempor sint esse voluptate cupidatat ea occaecat mollit consequat in irure ut nostrud. Nulla elit pariatur id aliqua labore officia consequat cillum id culpa. Cupidatat exercitation proident laborum dolor tempor laboris aute culpa ex duis minim laborum duis. Minim dolore occaecat incididunt elit voluptate velit elit labore ea dolore dolore ea.',
+      'Art Deco is more connected to our modern concept of wellness than perhaps realized at first blush—the aesthetic’s Greek influences evoke the glory days of baths-loving ancients who, no doubt, would appreciate the many well-designed niceties available at newly opening 50-story residence 25 Park Row in downtown Manhattan. Enter the 20,000-square-foot amenities floor, dubbed Park Row Club and designed by Andre Mellone, at the condo across from City Hall Park to see firsthand how the two concepts meld serenely.',
     _id: '5c62102a7f91c209db13584f',
     createdAt: '2019-02-12T00:15:38.940Z',
-    updatedAt: '2019-04-22T17:44:33.937Z',
+    updatedAt: '2019-05-09T18:13:25.036Z',
     __v: 0,
     id: '5c62102a7f91c209db13584f'
   },
@@ -86,12 +86,92 @@ const pressArticles = [
     url:
       'https://newyorkyimby.com/2018/12/skyline-views-from-the-top-of-cookfox-designed-25-park-row-in-lower-manhatan.html',
     body:
-      'Sint elit cillum ut ipsum eiusmod nulla sint duis. Esse dolor Lorem tempor consequat irure cillum irure sit excepteur. Velit excepteur aliquip laboris est magna non incididunt labore reprehenderit laborum dolor esse. Id ut aliquip mollit eiusmod in. Mollit amet commodo exercitation duis enim labore voluptate. Velit esse cillum ut dolore ea fugiat adipisicing tempor culpa ullamco ad.',
+      'YIMBY recently toured COOKFOX’s 25 Park Row, and snapped the sunset and skyline views from the 41st floor of the 49-story, 665-foot tall residential skyscraper. Standing on what would be a full-floor unit, the space will soon be home to some of the best views of both Lower Manhattan, and Midtown to the north. The tower is developed by L&M Development Partners and the J&R Music Store founders, while sales of the 110 units are being handled by Corcoran Sunshine Marketing Group.',
     _id: '5c62104c7f91c209db135852',
     createdAt: '2019-02-12T00:16:12.917Z',
-    updatedAt: '2019-04-24T16:07:08.350Z',
+    updatedAt: '2019-05-09T18:11:16.645Z',
     __v: 0,
     id: '5c62104c7f91c209db135852'
+  },
+  {
+    publisher: 'Curbed NY',
+    title: 'New looks at COOKFOX’s J&R-replacing Financial District skyscraper',
+    date: '2018-08-27T00:00:00.000Z',
+    url: 'https://ny.curbed.com/2018/8/27/17784970/financial-district-25-park-row-cookfox-rendering',
+    body:
+      'It’s been four years since Financial District stalwart J&R Music World closed, with a new\ntall tower, developed by L&M Development and the family that owned the famed electronics store, taking its place. In that time, the building has begun to take shape—\nYIMBY recently reported that construction had passed the halfway point—and now, the\ndevelopment team is unveiling new details, along with the first official renderings, for the project.\n\nA new teaser site for the project, which takes its name—25 Park Row—from its address,\nhas also launched, and its apartments will hit the market sometime this fall. Corcoran\nSunshine Marketing Group will handle sales for those 110 condos.',
+    _id: '5c620f987f91c209db135840',
+    createdAt: '2019-02-12T00:13:12.166Z',
+    updatedAt: '2019-05-09T18:23:12.221Z',
+    __v: 0,
+    id: '5c620f987f91c209db135840'
+  },
+  {
+    publisher: 'New York Yimby',
+    title: 'Sales Launch For COOKFOX’s 25 Park Row In Financial District, New Renderings Released',
+    date: '2019-02-04T00:00:00.000Z',
+    url:
+      'https://newyorkyimby.com/2019/02/sales-launch-for-cookfoxs-25-park-row-in-financial-district-new-renderings-released.html',
+    body:
+      '25 Park Row is making news again, as sales have recently launched for the 50-story residential skyscraper. New renderings have also been released showing the residential interiors of the 665-foot tall condominium tower above City Hall Park, as well as the surrounding cityscape. Designed by COOKFOX Architects, the project is being developed by the J&R Store founders and L&M Development Partners, and will yield 110 residences. Corcoran Sunshine Marketing Group is handling sale of the units, while the amenity spaces are being designed by Studio Mellone. The landscaping is being created by Starr Whitehouse.',
+    _id: '5cd4723d52213e0562cc3627',
+    createdAt: '2019-05-09T18:32:29.245Z',
+    updatedAt: '2019-05-09T18:32:29.283Z',
+    __v: 0,
+    id: '5cd4723d52213e0562cc3627'
+  },
+  {
+    publisher: 'New York Post',
+    title: 'NYC’s new buildings are embracing Roman-style arches',
+    date: '2019-04-10T04:00:00.000Z',
+    url: 'https://nypost.com/2019/04/10/nycs-new-buildings-are-embracing-roman-style-arches/',
+    body:
+      'Classical architecture, meet New York’s luxury high rises. Across the city, architects and developers are embracing loggias: open-air, gallery-style terraces with elegant arches and columns that would be right at home in Rome.\n\nNearby, at the 50-story, 110-unit 25 Park Row, which is opening in 2020, some units contain luxurious loggias with slatted roofs and green gardens overlooking downtown Manhattan. Prices range from $1.6 million for a one-bedroom to $12.5 million for a four-bedroom.',
+    _id: '5cd4733b52213e0562cc3628',
+    createdAt: '2019-05-09T18:36:43.599Z',
+    updatedAt: '2019-05-09T18:36:43.602Z',
+    __v: 0,
+    id: '5cd4733b52213e0562cc3628'
+  },
+  {
+    publisher: 'The Real Deal',
+    title: 'At the desk of: Rachelle Friedman',
+    date: '2019-02-01T04:00:00.000Z',
+    url: 'https://therealdeal.com/issues_articles/at-the-desk-of-rachelle-friedman/',
+    body:
+      'Rachelle Friedman — who grew up in Brooklyn and graduated from what was then known as the Polytechnic Institute of Brooklyn — is the “R” in the J&R. She and her husband, Joseph Friedman (the “J”), counted some of the biggest names in the music business as customers and regular visitors. But over the years, they also began accumulating buildings and pushing further into the real estate business.\n\nThis shovel is from the ground-breaking of 25 Park Row, which will house 110 units and has a projected sellout of $436 million. The condo, which is being designed by CookFox Architects, is the Friedmans’ biggest real estate undertaking to date. The company converted 15 Park Row into 300-plus rentals in the 1990s and is now developing 1 Park Row into a boutique condo building, though Friedman declined to reveal her partner or the timeline for the latter project.',
+    _id: '5cd4746d52213e0562cc3629',
+    createdAt: '2019-05-09T18:41:49.883Z',
+    updatedAt: '2019-05-09T18:41:49.886Z',
+    __v: 0,
+    id: '5cd4746d52213e0562cc3629'
+  },
+  {
+    publisher: 'The Wall Street Journal',
+    title: 'Former Site of J&R Electronics Store Now Houses $25 Million Penthouse',
+    date: '2019-05-15T00:00:00.000Z',
+    url:
+      'https://www.wsj.com/articles/former-site-of-j-r-music-and-computer-world-now-houses-25-million-penthouse-11557932464',
+    body:
+      'It’s the site that once housed the now-shuttered J&R Music and Computer World, a Manhattan institution known for selling albums and hosting free concerts and autograph signings. Now, a partnership that includes J&R’s founders is pitching a $25 million penthouse at the location, which has become a luxury condo development.\n\nRachelle Friedman, one of the founders of J&R, said she and her husband Joe Friedman purchased several of the buildings that housed their store on Park Row, near City Hall in Lower Manhattan, in the 1980s in order to protect themselves from future rent hikes. J&R shuttered in 2014, and the couple contributed two of those buildings to a partnership with L&M Development.',
+    _id: '5ceeae0352213e0562cc362a',
+    createdAt: '2019-05-29T16:06:27.177Z',
+    updatedAt: '2019-05-29T16:06:27.180Z',
+    __v: 0,
+    id: '5ceeae0352213e0562cc362a'
+  },
+  {
+    publisher: 'Curbed',
+    title: 'Lavish penthouse with views of City Hall Park asks $25M',
+    date: '2019-05-15T00:00:00.000Z',
+    url: 'https://ny.curbed.com/2019/5/15/18625020/penthouse-for-sale-in-nyc-financial-district',
+    body:
+      'An elegant duplex penthouse at 25 Park Row in the Financial District has just listed for $25 million. That tower, designed by COOKFOX, sits among landmark buildings with stunning views to the Woolworth building, One World Trade Center, and City Hall Park.\n\nThe 5,956-square-foot property has five bedrooms, five and a half bathrooms, a library, a fireplace, multiple terraces, and a double-height living room with an uninterrupted wall of windows.',
+    _id: '5ceeb05452213e0562cc362b',
+    createdAt: '2019-05-29T16:16:20.698Z',
+    updatedAt: '2019-05-29T16:16:20.702Z',
+    __v: 0,
+    id: '5ceeb05452213e0562cc362b'
   }
 ];
 
